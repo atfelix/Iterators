81a7023f76fa425cdcfd7a38f4977d6e7dc27a34
Chuck Grindel <chuck@chuckgrindel.com>
2020-08-29 14:58:28 -0600

    PeekingIteratorTest: Added test to demonstrate next() requiring peek() first. PeekingIterator: Updated next() to exercise peek() first.

--- Tests/IteratorsTests/PeekingIteratorTests.swift
+++ Tests/IteratorsTests/PeekingIteratorTests.swift
@@ -13,4 +13,13 @@ final class PeekingIteratorTests: XCTestCase {
         XCTAssertEqual(nil, iterator.peek())
         XCTAssertEqual(nil, iterator.next())
     }
+
+    func testPeekingIterator_WithoutPeek() {
+        // Make sure that iterator does not require peek to be called first.
+        var iterator = [1,2,3].makeIterator().peekable()
+        XCTAssertEqual(1, iterator.next())
+        XCTAssertEqual(2, iterator.next())
+        XCTAssertEqual(3, iterator.next())
+        XCTAssertEqual(nil, iterator.next())
+    }
 }
